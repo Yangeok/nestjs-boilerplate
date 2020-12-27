fd81959628c348e5bb8a67b14db47de79fe818ce
Yangwook Jeong <yangwookee@gmail.com>
2020-12-27 21:16:54 +0900

    Update root module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from './config/app/app.module';
 import { MysqlModule } from './config/database/mysql/mysql.module';
+import { RoutesControllers, RoutesServices } from './routes'
 
 @Module({
   imports: [AppConfigModule, MysqlModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, RoutesControllers],
+  providers: [AppService, RoutesServices],
 })
 export class AppModule {}
